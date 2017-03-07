21cbbc747efea3095aeaefd2cf85fa3cb87f8093
Tung-Trinh Thanh Tung <tungtt@vsii.com>
2017-03-07 09:13:52 +0700

    Change password

--- QLHS/ChangePassword.aspx
+++ QLHS/ChangePassword.aspx
@@ -2,4 +2,35 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <ul class="breadcrumb">
+        <li>
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Default.aspx">Home Page</asp:HyperLink>
+        </li>
+        <li class="active">
+            <asp:HyperLink ID="HyperLink2" runat="server" NavigateUrl="~/ChangePassword.aspx">Change Password</asp:HyperLink>
+        </li>
+    </ul>
+
+    <div class="col-md-7 box-center">
+        <div class="box">
+            <div class="box-body">
+                <div class="wrapper panel-body">
+                    <fieldset>
+                        <legend>Change Password</legend>
+                        <div class="form-group">
+                            <label>Current Password</label>
+                            <asp:TextBox ID="txtCurrentPassword" CssClass="form-control" runat="server" 
+                                TextMode="Password"></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                            <label>New Password</label>
+                            <asp:TextBox ID="txtNewPassword" CssClass="form-control" runat="server" 
+                                TextMode="Password"></asp:TextBox>
+                        </div>
+                    </fieldset>
+                    <asp:Button ID="btnChangePassword" runat="server" Text="Change Password" CssClass="btn aka-btn aka-btn-darkblue col-sm-12 btn-success" />
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- QLHS/ChangePassword.aspx.vb
+++ QLHS/ChangePassword.aspx.vb
@@ -1,8 +1,40 @@
-﻿Public Class ChangePassword
+﻿Imports System.Data.SqlClient
+Imports System.Web.Security
+Public Class ChangePassword
     Inherits System.Web.UI.Page
-
+    Dim con As SqlConnection
+    Dim cmd As SqlCommand
+    Dim cmdUpdate As SqlCommand
+    Dim dr As SqlDataReader
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    Protected Sub btnChangePassword_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnChangePassword.Click
+        Dim currentPassword As String = txtCurrentPassword.Text
+        Dim newPassword As String = txtNewPassword.Text
+        Dim userId As String = Session("UserID")
+        con = New SqlConnection("Data Source=TUNGTT-W7;Initial Catalog=QLSV;Integrated Security=SSPI")
+        con.Open()
+        cmd = New SqlCommand("SELECT password FROM users WHERE id=" + userId, con)
+        dr = cmd.ExecuteReader
+        If (dr.Read) Then
+            If (dr(0).ToString = currentPassword) Then
+                If (newPassword.Length < 6 Or newPassword.Length > 12) Then
+                    MsgBox("Password Is Too Short Or Too Long. Please Enter >= 6 Or <=12 characters ")
+                Else
+                    cmd.Dispose()
+                    dr.Close()
+                    cmdUpdate = New SqlCommand("UPDATE users SET password='" + newPassword + "' WHERE id = " + userId, con)
+                    cmdUpdate.ExecuteNonQuery()
+                    con.Close()
+                    Response.Redirect("Default.aspx")
+                End If
+            Else
+                MsgBox("Password Is Wrong. Re-enter Correct Password")
+            End If
+        Else
+            Response.Redirect("Logout.aspx")
+        End If
+    End Sub
 End Class
