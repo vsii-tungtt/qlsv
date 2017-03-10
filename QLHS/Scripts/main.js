8a3073ae7881231636b4fab586876c7fcd05a757
Tung-Trinh Thanh Tung <tungtt@vsii.com>
2017-03-10 13:53:05 +0700

    js

---
+++ QLHS/Scripts/main.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $(".numeric").keypress(function (event) {
+        // Backspace, tab, enter, end, home, left, right
+        // We don't support the del key in Opera because del == . == 46.
+        var controlKeys = [8, 9, 13, 35, 36, 37, 39];
+        // IE doesn't support indexOf
+        var isControlKey = controlKeys.join(",").match(new RegExp(event.which));
+        // Some browsers just don't raise events for control keys. Easy.
+        // e.g. Safari backspace.
+        if (!event.which || // Control keys in most browsers. e.g. Firefox tab is 0
+            (48 <= event.which && event.which <= 57) || (event.which == 46) || // Always 1 through 9
+            (48 == event.which && $(this).attr("value")) || // No 0 first digit
+            isControlKey) { // Opera assigns values for control keys.
+            return;
+        } else {
+            event.preventDefault();
+        }
+    });
+})
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
